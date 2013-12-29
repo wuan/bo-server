5d309600865216f60d93b9ecaed642695d740552
Andreas Würl <andi@tryb.de>
2013-12-29 22:39:24 +0100

    cleanup funkload service test case

--- tests/test_bo-webservice.py
+++ tests/test_bo-webservice.py
@@ -9,6 +9,7 @@ from random import random
 from funkload.FunkLoadTestCase import FunkLoadTestCase
 from funkload.utils import Data
 
+
 class BoWebservice(FunkLoadTestCase):
     """This test use a configuration file BlitzortungService.conf."""
 
@@ -19,48 +20,48 @@ class BoWebservice(FunkLoadTestCase):
 
     def call_service(self, method_name, *args):
         jsonrpc_data = {}
-	jsonrpc_data['id'] = 0
-	jsonrpc_data['method'] = method_name
-	jsonrpc_data['params'] = args
-	jsonrpc_data_string = json.dumps(jsonrpc_data)
-	self.logd("jsonrpc_data: '%s'" %(jsonrpc_data_string))
-	return self.post(self.server_url, params=Data('text/json', jsonrpc_data_string))
+        jsonrpc_data['id'] = 0
+        jsonrpc_data['method'] = method_name
+        jsonrpc_data['params'] = args
+        jsonrpc_data_string = json.dumps(jsonrpc_data)
+        self.logd("jsonrpc_data: '%s'" % (jsonrpc_data_string))
+        return self.post(self.server_url, params=Data('text/json', jsonrpc_data_string))
 
     def get_response(self):
         self.logd("jsonrpc_response: '%s'" % self.getBody())
         response = json.loads(self.getBody())
 
-	if 'fault' in response:
-  	    raise Exception(response['faultString'])
+        if 'fault' in response:
+            raise Exception(response['faultString'])
 
         return (response[0] if response and len(response) > 0 else None)
 
     def test_check(self):
-	ret = self.call_service('check')
+        ret = self.call_service('check')
         self.logd('ret: %s' % str(ret).strip())
-	response = self.get_response()
-	self.assert_('count' in response, 'response contains element "count"')
-	self.assert_(isinstance(response['count'], int), '"count" element has int type')
+        response = self.get_response()
+        self.assert_('count' in response, 'response contains element "count"')
+        self.assert_(isinstance(response['count'], int), '"count" element has int type')
 
     def test_cached(self):
-	ret = self.call_service('cached', 0)
+        ret = self.call_service('cached', 0)
         self.logd('ret: %s' % str(ret).strip())
-	response = self.get_response()
+        response = self.get_response()
 
     def test_get_strokes(self):
-	ret = self.call_service('get_strokes', 60)
+        ret = self.call_service('get_strokes', 60)
         self.logd('ret: %s' % str(ret).strip())
-	response = self.get_response()
+        response = self.get_response()
 
     def test_get_strokes_raster(self):
-	ret = self.call_service('get_strokes_raster', 60, 10000, 1)
+        ret = self.call_service('get_strokes_raster', 60, 10000, 1)
         self.logd('ret: %s' % str(ret).strip())
-	response = self.get_response()
+        response = self.get_response()
 
     def test_get_strokes_raster_uncached(self):
-	ret = self.call_service('get_strokes_raster_uncached', 60, 10000, 1)
+        ret = self.call_service('get_strokes_raster_uncached', 60, 10000, 1)
         self.logd('ret: %s' % str(ret).strip())
-	response = self.get_response()
+        response = self.get_response()
 
     def tearDown(self):
         """Setting up test."""
