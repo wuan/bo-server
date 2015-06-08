fe9a301f46d11e83b21a36dbfc8488bafe814809
Andreas Würl <andi@tryb.de>
2015-06-08 12:09:55 +0000

    fix cluster generation and crontab template

--- debian/cron.d
+++ debian/cron.d
@@ -10,9 +10,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #* * * * *	root	ps >/dev/null -C vacuumdb || bo-import && touch /tmp/.bo-import.last
 # calculate clusters from current stroke data
 #* * * * *       root    ps >/dev/null -C vacuumdb || (bo-import && touch /tmp/.bo-import.last) && (bo-cluster -m import && touch /tmp/bo-cluster.last)
-# insert current webservice log data
+# write webservice log to database
 #* * * * *       root    bo-webservice-insertlog >/dev/null
 # import raw data and add to local archive
 #15 * * * *	root	bo-import-raw &>/dev/null && touch /tmp/.bo-import-raw.last
-# write webservice log to database
-#*/5 * * * *	root	bo-webservice-insertlog 
