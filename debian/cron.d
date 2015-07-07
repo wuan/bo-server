a2f9ad67efba1c2db026dd75bca3d19efca48aaa
Andreas Wuerl <andi@tryb.de>
2015-07-07 16:24:04 +0000

    cleanup cron.d table

--- debian/cron.d
+++ debian/cron.d
@@ -6,11 +6,9 @@ SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # m h dom mon dow user  command
-# import stroke and station data and insert into local db
-#* * * * *	root	ps >/dev/null -C vacuumdb || bo-import && touch /tmp/.bo-import.last
-# calculate clusters from current stroke data
-#* * * * *       root    ps >/dev/null -C vacuumdb || (bo-import && touch /tmp/.bo-import.last) && (bo-cluster -m import && touch /tmp/bo-cluster.last)
+# import stroke and station data into local db and calculate clusters from current stroke data
+#* * * * *      root    ps >/dev/null -C vacuumdb || (bo-import && touch /tmp/.bo-import.last) && (bo-cluster -m import && touch /tmp/bo-cluster.last)
 # write webservice log to database
-#* * * * *       root    bo-webservice-insertlog >/dev/null
+#* * * * *      root    bo-webservice-insertlog >/dev/null
 # import raw data and add to local archive
-#15 * * * *	root	bo-import-raw &>/dev/null && touch /tmp/.bo-import-raw.last
+#15 * * * * 	root	bo-import-raw &>/dev/null && touch /tmp/.bo-import-raw.last
