ed58a955ee4aaa3c092851fd3be85d0ba6a58a7f
Andreas Würl <andi@tryb.de>
2015-10-29 06:30:58 +0000

    fixed timestamp file name

--- debian/cron.d
+++ debian/cron.d
@@ -7,7 +7,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # m h dom mon dow user  command
 # import stroke and station data into local db and calculate clusters from current stroke data
-#* * * * *      root    ps >/dev/null -C vacuumdb || (bo-import && touch /tmp/.bo-import.last) && (bo-cluster -m import && touch /tmp/bo-cluster.last)
+#* * * * *      root    ps >/dev/null -C vacuumdb || (bo-import && touch /tmp/.bo-import.last) && (bo-cluster -m import && touch /tmp/.bo-cluster.last)
 # write webservice log to database
 #* * * * *      root    bo-webservice-insertlog >/dev/null
 # import raw data and add to local archive
